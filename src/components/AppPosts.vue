6e6af472b5156a534cab9a34136d95869feb53c2
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-10-14 18:07:46 +0200

    [UPD] visualizzo 6 cards

--- src/components/AppPosts.vue
+++ src/components/AppPosts.vue
@@ -14,7 +14,9 @@ export default {
     methods: {
         getPosts(){
             axios.get('http://127.0.0.1:8000/api/posts').then((response) => {
-                this.posts = response.data.results;
+                console.log(response.data.results);
+                // this.posts = response.data.results; // questo se nel backend abbiamo messo Post::all();
+                this.posts = response.data.results.data;
             });
         }
     }
@@ -30,11 +32,11 @@ export default {
                 <div class="row">
                     <div class="col-4" v-for="post in posts" :key="post.id">
                         <div class="card">
-                            <img class="card-img-top" :src="post.cover_image.startsWith('http') ?post.cover_image : `http://127.0.0.1:8000/storage/${post.cover_image}`">
+                            <img class="card-img-top" :src="post.cover_image.startsWith('http') ? post.cover_image : `http://127.0.0.1:8000/storage/${post.cover_image}`">
                             <div class="card-body">
                                 <h3 class="card-title">{{ post.title }}</h3>
                                 <p>
-                                    {{ post.content }}
+                                    {{ post.content.substr(0, 100) + '...' }}
                                 </p>
                             </div>
                         </div>
