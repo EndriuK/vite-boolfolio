ec45a6898f9661977dc4d09f65be96853c52c9f8
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-10-19 12:21:52 +0200

    [UPD] creata form

---
+++ src/pages/PageContacts.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <div class="my-5">
+        <div class="container">
+            <div class="row gy-5">
+                <div class="col-12">
+                    <h1 class="text-center">Contatti</h1>
+                </div>
+                <div class="col-12">
+                    <form method="post" @submit.prevent="sendForm()">
+                        <div class="row gy-3">
+                            <div class="col-12 col-md-6">
+                                <label class="control-label">
+                                    Nome
+                                </label>
+                                <input type="text" class="form-controller" name="name" id="name" placeholder="Nome">
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <label class="control-label">
+                                    Cognome
+                                </label>
+                                <input type="text" class="form-controller" name="surname" id="surname" placeholder="Cognome">
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <label class="control-label">
+                                    Email
+                                </label>
+                                <input type="text" class="form-controller" name="email" id="email" placeholder="Email">
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <label class="control-label">
+                                    Telefono
+                                </label>
+                                <input type="text" class="form-controller" name="phone" id="phone" placeholder="Telefono">
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <label class="control-label">
+                                    Messaggio
+                                </label>
+                                <textarea type="text" class="form-controller" name="content" id="content" placeholder="Messaggio"></textarea>
+                            </div>
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-sm btn-success">Invia</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
